f571575cc62085ec421770cb2f5cacacd4fbcd47
Joseph.M.Dumdum.Jr <146814030+Joseph-Dumdum@users.noreply.github.com>
2024-06-05 13:05:17 +0800

    delete.php
    Update

---
+++ Controller/home/delete.php
@@ -0,0 +1,25 @@
+<?php
+
+isUserValid();
+
+$conn = new Database( config() );
+
+try {   
+    $conn->query("DELETE FROM history WHERE id = :id AND user_id = :user_id", [
+
+        'id' => $_POST['id'],
+
+        'user_id' => $_SESSION['user_id']
+
+    ]);
+}
+  
+catch (Exception $e) {
+  
+    errorPage(401, "Unauthorized");;
+  
+} finally {
+   
+  redirect('/');
+
+}
