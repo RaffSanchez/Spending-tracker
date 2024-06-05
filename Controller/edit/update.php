2c5b8ce8db982857099f573263c0c14014b332a0
importsMj <146841659+importsMj@users.noreply.github.com>
2024-06-05 17:24:33 +0800

    Update of controller/edit/update.php
    updated version of controller/edit/update.php

---
+++ Controller/edit/update.php
@@ -0,0 +1,28 @@
+<?php
+
+isUserValid();
+
+$conn = new Database( config() );
+
+try {
+
+    $conn->query("  UPDATE history 
+                    SET product_type = :product_type, product_name = :product_name, product_price = :product_price 
+                    WHERE id = :id AND user_id = :user_id", [
+                                    
+                    "product_type" => $_POST['type'],
+                    "product_name" => $_POST['product'],
+                    "product_price" => $_POST['price'],
+                    "id" => $_POST['id'],
+                    "user_id" => $_SESSION['user_id']
+    ]);
+
+}catch (Exception $e) {
+
+    errorPage(401, "Unauthorized");
+
+}finally {
+
+    redirect('/');
+
+}
