16bb04b30ed0efc0c7e51d28f489ed9a4ec44dd5
Joseph.M.Dumdum.Jr <146814030+Joseph-Dumdum@users.noreply.github.com>
2024-06-05 13:06:40 +0800

    index.php
    Update

---
+++ Controller/home/index.php
@@ -0,0 +1,31 @@
+<?php
+
+isUserValid();
+
+$conn = new Database( config() );
+
+$result = $conn->query("SELECT * FROM history WHERE date_created = :date_created AND user_id = :user_id", [ 
+
+    "date_created" => date("Y/m/d"),
+
+    "user_id" => $_SESSION['user_id'] 
+
+])->fetchAll();
+
+$total = 0;
+
+foreach ($result as $row)
+
+{
+
+    $total += $row['product_price'];
+
+}
+
+view("index.view.php",[
+
+    'result' => $result,
+
+    'total' => $total
+
+]);
