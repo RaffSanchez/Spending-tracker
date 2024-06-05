bd9b29fbad12aa981b2921ea51ccd2adf8055960
RaffSanchez <raffy.sanchez.cartel@gmail.com>
2024-06-05 10:36:53 +0800

    new data

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php>
+<?php
+
+session_start();
+
+$uri =  parse_url( $_SERVER['REQUEST_URI'] )['path'];
+$file = explode( "/", $uri )[1];
+
+require 'Core/Router.php';
+require 'Core/Validator.php';
+require 'Core/function.php';
+require 'Core/Database.php';
+
+$router = new Router();
+
+require 'routes.php';
+$method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
+$router->route( $uri, $method );
