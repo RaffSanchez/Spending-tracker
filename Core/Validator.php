53a4172804bb16d4bbf74c64a0da75dc146cd081
JPAULG5 <151696615+JPaulG5@users.noreply.github.com>
2024-06-09 10:19:58 +0800

    Validator.php

--- Core/Validator.php
+++ Core/Validator.php
@@ -1,1 +1,35 @@
-<?php
+<?php<?php
+
+class Validator
+{
+
+    public static function string( $value, $min = 1, $max = INF )
+    {
+        $value = trim(htmlspecialchars($value));
+        
+        if(Validator::number($value)) return false;
+
+        return strlen($value) >= $min && strlen($value) <= $max;
+    }
+
+    public static function number( $value )
+    {
+        return filter_var($value, FILTER_VALIDATE_INT);
+    }
+
+    public static function email( $value )
+    {
+        return filter_var($value, FILTER_VALIDATE_EMAIL);
+    }
+
+    public static function isUserExist( $value, $compare )
+    {
+        return $value === $compare;
+    }
+
+    public static function isPasswordExist( $user_password, $db_password )
+    {
+        return password_verify($user_password, $db_password);
+    }
+
+}
