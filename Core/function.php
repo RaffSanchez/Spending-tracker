888ca2caff2123fb21dbd6d90700dba3c411262e
JPAULG5 <151696615+JPaulG5@users.noreply.github.com>
2024-06-09 10:20:14 +0800

    function.php

--- Core/function.php
+++ Core/function.php
@@ -1,1 +1,59 @@
-<?php
+<?php<?php
+
+
+function dd ( $value )
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+    die();
+
+}
+
+function redirect( $path )
+{
+    header("location: /{$GLOBALS['file']}/index.php{$path}");
+    die();
+}
+
+function view( $path, $attribute = [] )
+{
+    extract( $attribute );
+    require 'Views/' . $path;
+}
+
+function config( $configName = 'database' )
+{
+
+    $config = require 'config.php';
+    return $config[$configName];
+}
+
+function urlIs($value){
+    
+    return parse_url($_SERVER["REQUEST_URI"])['path'] == $value;
+
+}
+
+function isUserValid() {
+    
+    if( empty($_SESSION) ) redirect("/login");
+
+}
+
+function redirectLink( $path )
+{
+    return "/{$GLOBALS['file']}/index.php/{$path}";
+}
+
+
+function errorPage( $code, $text )
+{
+    view("error/404.view.php", [
+        "error" => $code,
+        "description" => $text,
+        "header" => "location: /{$GLOBALS['file']}/index.php"
+    ]);
+
+    ;
+}
