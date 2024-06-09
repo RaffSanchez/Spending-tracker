b02b537750e09104963e702f670d6fa6541df242
JPAULG5 <151696615+JPaulG5@users.noreply.github.com>
2024-06-09 10:18:46 +0800

    Database.php

--- Core/Database.php
+++ Core/Database.php
@@ -1,1 +1,43 @@
-<?php
+<?php<?php
+
+class Database {
+    public $conn;
+    public $statement;
+
+    public function __construct( $config, $username = "root", $password = "")
+    {
+        $dns = 'mysql:' . http_build_query( $config, '', ';' );
+
+        $this->conn = new PDO( $dns, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    public function query( $statement, $param = [])
+    {
+        $this->statement = $this->conn->prepare( $statement );
+
+        $this->statement->execute( $param );
+
+        return $this;
+    }
+
+    public function fetchAll()
+    {
+        return $this->statement->fetchAll();
+    }
+
+    public function fetch()
+    {
+        return $this->statement->fetch();
+    }
+
+    public function findOrFail( $redirect = '/login'){
+        $result = $this->fetch();
+    
+        if( !$result ) redirect( $redirect );
+
+        return $result;
+    }
+
+}
