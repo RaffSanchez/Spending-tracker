27ec2149719782be7229698f15434bf5764bec88
JPAULG5 <151696615+JPaulG5@users.noreply.github.com>
2024-06-09 10:48:25 +0800

    index.php

---
+++ Controller/history/index.php
@@ -0,0 +1,23 @@
+<?php
+
+isUserValid();
+
+$conn = new Database( config() );
+
+try {
+
+    $result = $conn->query("SELECT * FROM history WHERE user_id = :user_id AND date_created < :date_created ORDER BY date_created DESC", [
+        'user_id' => $_SESSION['user_id'],
+        'date_created' => date("Y/m/d")
+    ])->fetchAll();
+
+
+    
+}catch (Exception $e) {
+
+    redirect("/history");
+}
+
+view('history.view.php', [
+    'data' => $result
+]);
